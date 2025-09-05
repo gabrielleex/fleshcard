efd1b46de3b751c7e1bf6a510c07299f049c956d
Gabyx <gabrielle.matias.jesus@escola.pr.gov.br>
2025-09-05 12:21:14 -0300

    Update Pergunta.js

--- Pergunta.js
+++ Pergunta.js
@@ -1,5 +1,16 @@
 criaCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’
+    ‘programação’,
+    ‘O que é JavaScript?’  
+    ‘O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web,
+    ‘permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse. ’
+)
+criaCartao(
+  ‘matemática‘,
+  ‘O que é um número primo?‘,
+  ‘Um número primo é um número natural maior que 1 que não pode ser dividido exatamente por nenhum outro número, exceto 1 e ele mesmo.‘
+)
+criaCartao(
+  ‘História‘,
+  ‘O que foi a Revolução Francesa?‘,
+  ‘A Revolução Francesa foi um movimento social e político que aconteceu na França entre 1789 e 1799, onde a monarquia foi abolida e a República foi proclamada.‘
 )
